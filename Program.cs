142501c5ac2e6317bc75f6b642b2129eb796c171
M.Israr <israrbaig557799@gmail.com>
2025-04-13 15:07:22 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+namespace Helloworld{
+ class check{ 
+         static void Main( String [] args)
+         {
+          int a =50;                   // For interger numbers
+          float pi = 3.14f;            // For floating point numbers
+          double salary= 4500.76d;     // For decimal numbers
+
+          // Prints the values of the variables
+
+          System.Console.WriteLine($"The value of a is {a}");
+          System.Console.WriteLine($"The value of pi is {pi}");
+          System.Console.WriteLine($"The value of salary is {salary}");
+
+          Console.ReadKey();
+         }
+
+    }
+ }
+
